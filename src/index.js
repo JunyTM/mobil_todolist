bce2704debc821baf3f7f25e2210d48b0ef94d56
ManhKim <manhtokim@gmail.com>
2022-09-20 15:21:20 +0700

    check dependencies

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ const styles = StyleSheet.create({
         width: 100,
         height: 100,
 
-        marginTop: 50,
+        marginTop: 50, 
 
         backgroundColor: 'deepskyblue',
         alignSelf: 'center',
